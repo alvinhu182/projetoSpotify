1ab531a61f869d4f81d668060971babd740ffac3
alvinhu182 <alvinhu@hotmail.com>
2025-05-11 20:54:17 -0300

    ajustes para o deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,11 @@
 // Fetch ou Axios
+import "dotenv/config";
 import axios from "axios";
 
-// const { NODE_ENV } = process.env;
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 // const URL = "http://localhost:3001/api";
-const URL = "https://deploy-jornada-full-stack.onrender.com/api";
+//const URL = "https://deploy-jornada-full-stack.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
